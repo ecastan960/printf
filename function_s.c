3e60cf11044088ebd003e2379bb28d1e6d70f0ac
ecastan960 <ecastan1@eafit.edu.co>
2020-10-30 18:29:14 +0000

    fix null

--- function_s.c
+++ function_s.c
@@ -12,9 +12,12 @@ int f_s(va_list arg_string)
 	char *string;
 
 	string = va_arg(arg_string, char *);
-	for (a = 0; string[a]; a++)
+	if (string != NULL)
 	{
-		contador_fs = contador_fs + _putchar(string[a]);
+		for (a = 0; string[a]; a++)
+		{
+			contador_fs = contador_fs + _putchar(string[a]);
+		}
 	}
 return (contador_fs);
 }
