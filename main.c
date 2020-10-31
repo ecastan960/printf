0e46643b93b3b309e684eb00a1f7d022953148ba
ecastan960 <ecastan1@eafit.edu.co>
2020-10-31 02:48:57 +0000

    try again

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <limits.h>
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+    _printf("Complete the sentence: You %s nothing, Jon Snow.\n", (char *)0);
+    return (0);
+}
